f3521cd2c10637deeca1b714018eeaebb4e0d215
maegop <megordon@alu.ucam.edu>
2022-03-03 19:41:54 +0100

    added images in a imported file to easy use

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+
+import { images } from "../../constants";
 import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div>
+        <img src={images.logo} alt="logo" />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
